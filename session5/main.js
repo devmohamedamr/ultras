b67cdc0a3aecdcc2c19e6d1d4683f8ea0aa7ddac
mohamed amr <dev.mohamedamr@gmail.com>
2021-09-11 15:31:13 +0200

    5

---
+++ session5/main.js
@@ -0,0 +1,65 @@
+// var Username = 'mohamed';
+// var uSername = 'eslam';
+// var usErname = 'ahmed';
+
+
+// alert(username);
+
+// console.log(username);
+
+// document.write(username);
+
+// console.log(console);
+
+
+
+// console.log(10 < 20);
+
+
+// if(10 < 20){
+//     console.log("ahmed");
+// }else if(10 < 30){
+//     console.log("mohamed");
+// }else{
+//     console.log("error");
+// }
+
+// var x =true;
+// console.log(typeof(x));
+
+// switch(x){
+//     case 'ahm ed':
+//         console.log("ahmed");
+//     break;
+//     case 'mohamed':
+//         console.log("mohamed");
+//     break;
+//     default:
+//         console.log("errro");
+// }
+
+
+
+// var i = 0;
+
+// do{
+//     console.log("test");
+//     i++;
+// }while(i>10);
+
+
+// for(var i = 1;i<15;i++){
+
+//     if(i == 5){
+//         document.write("mohamed <br>");
+//         continue;
+//     }
+//         document.write(i + "<br>");
+//     // }
+// }
+
+
+
+
+
+
